aa1b70c4dbcb626098c839ab3fd0c0f9537d252c
PlanC90 <ceyhun.yilmaz@hotmail.com.tr>
2024-12-22 23:02:48 +0300

    Silinen klasörleri GitHub'dan kaldır

--- src/config/bot.js
+++
@@ -1,8 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-export const BOT_CONFIG = {
-    token: process.env.BOT_TOKEN,
-    username: process.env.BOT_USERNAME,
-    mainGroup: process.env.MAIN_GROUP
-};
--- src/routes/index.js
+++
@@ -1,7 +0,0 @@
-import { userRoutes } from './userRoutes.js';
-import { withdrawalRoutes } from './withdrawalRoutes.js';
-
-export function setupRoutes(app) {
-    app.use('/api/users', userRoutes);
-    app.use('/api/withdrawals', withdrawalRoutes);
-}
