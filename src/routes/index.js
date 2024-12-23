58b7b50e976719dd54d4872974a77c0abe72c48f
PlanC90 <ceyhun.yilmaz@hotmail.com.tr>
2024-12-23 12:35:41 +0300

    Silinen klasörleri GitHub'dan kaldır

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const { userRoutes } = require('./userRoutes');
-const { withdrawalRoutes } = require('./withdrawalRoutes');
-
-function setupRoutes(app) {
-    app.use('/api/users', userRoutes);
-    app.use('/api/withdrawals', withdrawalRoutes);
-}
-
-module.exports = { setupRoutes };
