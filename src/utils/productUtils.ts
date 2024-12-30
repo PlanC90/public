c17f4c8ecd952378ab3fbe019b77c1bc09889110
PlanC90 <ceyhun.yilmaz@hotmail.com.tr>
2024-12-30 15:45:42 +0300

    Silinen klasörleri GitHub'dan kaldır

--- src/utils/productUtils.ts
+++
@@ -1,6 +0,0 @@
-import { products } from '../data/products';
-import { Product } from '../types/product';
-
-export const findProductByBarcode = (barcode: string): Product | null => {
-  return products.find(p => p.barcode === barcode) || null;
-};
