10b4cd180d33b14267322197e7f2d9020d22d822
PlanC90 <ceyhun.yilmaz@hotmail.com.tr>
2025-01-03 00:33:34 +0300

    Silinen klasörleri GitHub'dan kaldır

--- src/js/utils/walletUrls.js
+++
@@ -1,8 +0,0 @@
-// Configuration object for wallet download URLs
-export const WALLET_URLS = {
-  android: 'https://xepmeme.glitch.me/cuzdan.html',
-  ios: 'https://xepmeme.glitch.me/cuzdan.html',
-  windows: 'https://github.com/ElectraProtocol/XEP-Core/releases/download/v1.0.5.0/Win64-1.0.5.0-setup.exe',
-  mac: 'https://github.com/ElectraProtocol/XEP-Core/releases/download/v1.0.5.0/MacOS_1.0.5.0_QT.dmg',
-  unknown: 'https://xepmeme.glitch.me/cuzdan.html'
-};
