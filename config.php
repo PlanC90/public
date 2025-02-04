3ef110d2283cf3d00393bb5519215982be5e2a03
PlanC90 <ceyhun.yilmaz@hotmail.com.tr>
2025-02-05 02:14:19 +0300

    Silinen klasörleri GitHub'dan kaldır

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    return [
-        'db_host' => 'localhost', // Veritabanı sunucu adresi
-        'db_user' => 'your_db_user', // Veritabanı kullanıcı adı
-        'db_password' => 'your_db_password', // Veritabanı şifresi
-        'db_name' => 'your_db_name', // Veritabanı adı
-    ];
-    ?>
